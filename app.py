5883c0661fc60234a8b6221f583c7154f09ddf85
Eugene Triguba <eugenetriguba@gmail.com>
2019-11-03 01:23:54 -0500

    Organize dotfile structure, simplify app.py, and add Brewfile

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 import subprocess
+import distro
 
 
-def determine_operating_system():
-    print("Supported Operating Systems")
-    print("1. Ubuntu")
-    print("2. macOS")
-    os = int(input("OS to setup (number): "))
-    
-    if os == 1:
-        return "ubuntu"
-    elif os == 2:
-        return "macos"
-    else:
-        print(f"{os} is not a supported option.")
-        
+system = distro.linux_distribution(full_distribution_name=False)[0]
 
-def run_script(path: str):
-    subprocess.call([path])
-
-
-if __name__ == "__main__": 
-    os = determine_operating_system()
-    run_script(f"./operating_systems/{os}.sh")
+if system == 'darwin':
+    subprocess.call(['./dotfiles/macOS/install.sh'])
+elif system == 'ubuntu':
+    subprocess.call(['./dotfiles/Ubuntu/install.sh'])
+else:
+    print(system + " is not supported.")
